ae848eff3ae36b26f61da42ba543d19859099e67
KAISERaustin <3ak333@gmail.com>
2025-04-07 20:03:35 -0500

    Added different sized textures, resized level, character movement, added pipe creation logic, added main menu

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var character_body_2d: CharacterBody2D = $"."
 @onready var time_to_rotate_down: Timer = $time_to_rotate_down
+@onready var audio_jump: AudioStreamPlayer2D = $jump
+@onready var ground_death: AudioStreamPlayer2D = $ground_death
+@onready var load_up_main_menu: Timer = $load_up_main_menu
 
 var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -21,6 +24,7 @@ func _physics_process(delta: float) -> void:
 	#grabs the input, checks if on the ceiling, checks if they collided with a forbidden area!
 	if Input.is_action_just_pressed("jump") and not is_on_ceiling() and not is_a_loser:
 		animation_player.play("default")
+		audio_jump.play()
 		character_body_2d.rotation_degrees = -20
 		velocity.y = JUMP_VELOCITY
 		time_to_rotate_down.start()
@@ -37,15 +41,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	  
-func _ready() -> void:
-	pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("ground"):
+		ground_death.play()
+		animated_sprite_2d.stop()
 		is_a_loser = true
 		SPEED = 0
+		load_up_main_menu.start()
+		
 		
 func _on_time_to_rotate_down_timeout() -> void:
 	animation_player.play("falling_for_long_time") 
 	ready_to_rotate_down = true
+
+
+func _on_load_up_main_menu_timeout() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
